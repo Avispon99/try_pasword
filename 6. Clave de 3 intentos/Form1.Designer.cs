fa07134f89f1a6f7b7876c51b5e9d09f4babb4f1
Jhonatan Zu <jhonatan.zero@gmail.com>
2019-08-24 22:47:17 -0500

    fiirst

---
+++ 6. Clave de 3 intentos/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace _6.Clave_de_3_intentos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.TextBox();
+            this.clave = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.inicio = new System.Windows.Forms.Button();
+            this.cuenta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Autor: Jhonatan Leonardo Zuluaga";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fecha 19/11/2017";
+            // 
+            // id
+            // 
+            this.id.Location = new System.Drawing.Point(197, 96);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(100, 20);
+            this.id.TabIndex = 2;
+            // 
+            // clave
+            // 
+            this.clave.Location = new System.Drawing.Point(197, 157);
+            this.clave.Name = "clave";
+            this.clave.PasswordChar = 'x';
+            this.clave.Size = new System.Drawing.Size(100, 20);
+            this.clave.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(99, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "USUARIO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(85, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "CONTRASEÑA";
+            // 
+            // inicio
+            // 
+            this.inicio.Location = new System.Drawing.Point(146, 215);
+            this.inicio.Name = "inicio";
+            this.inicio.Size = new System.Drawing.Size(95, 23);
+            this.inicio.TabIndex = 6;
+            this.inicio.Text = "Iniciar Sesión ";
+            this.inicio.UseVisualStyleBackColor = true;
+            this.inicio.Click += new System.EventHandler(this.inicio_Click);
+            // 
+            // cuenta
+            // 
+            this.cuenta.AutoSize = true;
+            this.cuenta.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.cuenta.Location = new System.Drawing.Point(12, 47);
+            this.cuenta.Name = "cuenta";
+            this.cuenta.Size = new System.Drawing.Size(13, 13);
+            this.cuenta.TabIndex = 7;
+            this.cuenta.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 260);
+            this.Controls.Add(this.cuenta);
+            this.Controls.Add(this.inicio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.clave);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Clave de 3 intentos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.TextBox clave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button inicio;
+        private System.Windows.Forms.Label cuenta;
+    }
+}
+
---
+++ 6. Clave de 3 intentos/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6.Clave_de_3_intentos
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void inicio_Click(object sender, EventArgs e)
+        {
+
+            int count = int.Parse(cuenta.Text);
+
+
+            if (id.Text == "Jhonatan" && clave.Text == "Cinco")
+            {
+                if (cuenta.Text != "3" )
+                {
+                    MessageBox.Show(" BIENVENIDO SEÑOR JHONATAN.");
+                }
+                else { MessageBox.Show(" La cuenta ha sido BLOQUEADA por superar los 3 Intentos."); }
+                
+            }
+            else
+            {
+                if (cuenta.Text != "3")
+                {
+                    MessageBox.Show(" Contraseña o ID Errada.");
+                    count++;
+                    cuenta.Text = count.ToString();
+                }
+                else { MessageBox.Show(" La cuenta ha sido BLOQUEADA por superar los 3 Intentos."); }
+
+            }
+
+
+
+
+
+        }
+
+
+       
+    }
+}
